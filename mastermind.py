1346de7edf2e378e264c5d649432ff33eaf7a1eb
Adam <adam.narjis@student.hu.nl>
2020-02-13 11:37:18 +0100

    ini

--- mastermind.py
+++ mastermind.py
@@ -1,1 +1,28 @@
-print("Welkom bij Mastermind")
+import random
+def secret_input():
+    password = []
+    while len(password) < 4:
+        secret = int(input("Geef een getal tussen de 0 en 6: "))
+        if secret > 6:
+            print(" nder de 7 graag.")
+        elif secret <= 6:
+            password.append(secret)
+    print(password)
+# Menu
+keuze = input("Welkom bij Mastermind! Kies een gamemode. \n[a] Gokken \n[b] Gamemaster \n")
+if keuze.lower() == "a":
+    print("Begin met gokken")
+if keuze.lower() == "b":
+    secret_input()
+
+
+# def secret():
+#     passw = []
+#     while len(passw) < 4:
+#         passw.append(random.randint(0, 6))
+#     print(passw)
+# secret()
+
+# def possibilities():
+#     poss_list = []
+# possibilities()
